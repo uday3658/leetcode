1f6ecf1aad0e12e0850684d52cdb42502872bde4
uday3658 <udaypatel2209@gmail.com>
2026-07-11 00:45:22 +0530

    Time: 27 ms (24.28%), Space: 13.2 MB (45.64%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,14 +5,18 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
-            return False
-        dict1=dict()
-        dict2=dict()
+        # if len(s) != len(t):
+        #     return False
+        # dict1=dict()
+        # dict2=dict()
 
-        for i in range(len(s)):
-            dict1[s[i]]=dict1.get(s[i],0)+1
-            dict2[t[i]]=dict2.get(t[i],0)+1
+        # for i in range(len(s)):
+        #     dict1[s[i]]=dict1.get(s[i],0)+1
+        #     dict2[t[i]]=dict2.get(t[i],0)+1
 
-        return dict1==dict2
+        # return dict1==dict2
+
+        return sorted(s)==sorted(t)
+
+        
         
