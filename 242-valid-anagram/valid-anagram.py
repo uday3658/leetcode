705c7242d7a32487092283b37d494dd53ccfbec8
uday3658 <udaypatel2209@gmail.com>
2026-07-11 00:41:14 +0530

    Time: 23 ms (50.89%) | Memory: 14.2 MB (15.36%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        dict1=dict()
+        dict2=dict()
+
+        for i in range(len(s)):
+            dict1[s[i]]=dict1.get(s[i],0)+1
+            dict2[t[i]]=dict2.get(t[i],0)+1
+
+        return dict1==dict2
+        
