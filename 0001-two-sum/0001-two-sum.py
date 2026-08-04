1a878d0fb4d777d5d48bd6d52b266ea1e7a2c934
uday3658 <udaypatel2209@gmail.com>
2026-08-04 21:43:57 +0530

    Time: 2169 ms (15.63%), Space: 13.3 MB (36.15%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        # seen = {}
+
+        # for i, num in enumerate(nums):
+        #     complement = target - num
+
+        #     if complement in seen:
+        #         return [seen[complement], i]
+
+        #     seen[num] = i
+
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
